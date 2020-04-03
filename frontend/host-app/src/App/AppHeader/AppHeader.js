963454846514a212a6f0bc937a4cb8df9b636f24
demetoir-destop <wnsqlehlswk@naver.com>
2020-04-03 18:43:57 +0900

    refactoring: frontend host-app AppHeader 컴포넌트 파일 분리
    * add todos

--- frontend/host-app/src/App/AppHeader/AppHeader.js
+++ frontend/host-app/src/App/AppHeader/AppHeader.js
@@ -1,12 +1,9 @@
 import React from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
 import {makeStyles} from "@material-ui/core/styles";
-import HeaderAccountAvatar from "./HeaderAccountAvatar.js";
-import HeaderConfigAvatar from "./HeaderConfigAvatar.js";
-import EventSettingModal from "../../components/EventSettingModal/EventSettingModal.js";
-import useModal from "../../customhook/useModal.js";
+import AppHeaderTitle from "./AppHeaderTitle.js";
+import AppHeaderButtonArea from "./AppHeaderButtonArea.js";
 
 const useStyles = makeStyles(() => ({
 	header: {
@@ -18,23 +15,16 @@ const useStyles = makeStyles(() => ({
 	},
 }));
 
+
 function AppHeader() {
-	const [settingModalOpen, handleOpen, handleClose] = useModal();
 	const classes = useStyles(undefined);
 
 	return (
 		<AppBar position="static">
 			<Toolbar variant="dense" className={classes.header}>
-				<Typography variant="h6">바글바글</Typography>
+				<AppHeaderTitle/>
 				<div className={classes.rightSide}>
-					<HeaderConfigAvatar onClick={handleOpen} />
-					{settingModalOpen && (
-						<EventSettingModal
-							open={settingModalOpen}
-							handleClose={handleClose}
-						/>
-					)}
-					<HeaderAccountAvatar/>
+					<AppHeaderButtonArea/>
 				</div>
 			</Toolbar>
 		</AppBar>
