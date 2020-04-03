dfa80cb07c7762215608912297f426f6168cbfdc
demetoir-destop <wnsqlehlswk@naver.com>
2020-04-03 14:16:36 +0900

    refactoring: express host auth middleware
    * major refactoring
    * add test
    * add module sinon, @types/jsonwebtoken

--- backend/node/express/middleware/hostAuth.js
+++ backend/node/express/middleware/hostAuth.js
@@ -6,21 +6,56 @@ import config from "../config";
 
 const {tokenArgs, routePage} = config;
 
+const cookieKey = CookieKeys.HOST_APP;
+
+/**
+ *
+ * @param req {object}
+ * @return {*|ResponseCookie}
+ */
+function parseHostJWt(req) {
+	if (!(cookieKey in req.cookies)) {
+		throw Error(`cookie key '${cookieKey}' not found in request cookies`);
+	}
+
+	return req.cookies[cookieKey];
+}
+
+/**
+ *
+ * @param hostOauthId {string}
+ * @return {Promise<void>}
+ */
+async function verifyHost(hostOauthId) {
+	const isExist = await isExistHostOAuthId(hostOauthId);
+
+	if (!isExist) {
+		throw Error(`host's OauthId ${hostOauthId} is not verified`);
+	}
+}
+
+/**
+ *
+ * @param req {object}
+ * @param res {object}
+ * @param next {function}
+ * @return {Promise<*>}
+ */
 export default async function hostAuthenticate(req, res, next) {
 	try {
+		const hostJwt = parseHostJWt(req);
 		const payload = jwt.verify(
-			req.cookies[CookieKeys.HOST_APP],
+			hostJwt,
 			tokenArgs.secret,
 		);
-		const isExist = await isExistHostOAuthId(payload.sub);
 
-		if (isExist) {
-			res.redirect(routePage.host);
-		}
+		const hostOauthId = payload.sub;
 
-		return next();
+		await verifyHost(hostOauthId);
+
+		return res.redirect(routePage.host);
 	} catch (e) {
-		logger.error(e);
+		logger.error(e, e.stack);
 		return next();
 	}
 }
