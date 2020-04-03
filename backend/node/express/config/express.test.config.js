346d3d478406496be8b6aae60200ab4a9eb0ea31
demetoir-destop <wnsqlehlswk@naver.com>
2020-04-03 14:05:57 +0900

    add: express test config

--- backend/node/express/config/express.test.config.js
+++ backend/node/express/config/express.test.config.js
@@ -7,6 +7,16 @@ const env = process.env;
 const config = {
 	port: env.EXPRESS_TEST_PORT,
 	publicPath: env.EXPRESS_TEST_PUBLIC_PATH,
+	tokenArgs: {
+		secret: "process.env.AUTH_TOKEN_SECRET",
+		issuer: "process.env.AUTH_TOKEN_ISSUER",
+		audience: "process.env.AUTH_TOKEN_AUDIENCE",
+	},
+	routePage: {
+		main: "process.env.DEV_MAIN_PAGE",
+		host: "process.env.DEV_HOST_PAGE",
+		guest: "process.env.DEV_GUEST_PAGE",
+	},
 };
 
 export default config;
