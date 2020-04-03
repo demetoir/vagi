0f58879cd23489fbf6720e8d6a047d81af7bd941
demetoir-destop <wnsqlehlswk@naver.com>
2020-04-03 15:55:33 +0900

    refactoring: express host auth middleware
    * validator, error class 파일 분리

--- backend/node/express/middleware/hostAuth.js
+++ backend/node/express/middleware/hostAuth.js
@@ -1,38 +1,11 @@
 import jwt from "jsonwebtoken";
-import CookieKeys from "../CookieKeys.js";
-import {isExistHostOAuthId} from "../../DB/queries/host.js";
 import logger from "../logger.js";
 import config from "../config";
+import parseHostJWt from "./parseHostJWt.js";
+import verifyHostByOauthId from "./verifyHostByOauthId.js";
 
 const {tokenArgs, routePage} = config;
 
-const cookieKey = CookieKeys.HOST_APP;
-
-/**
- *
- * @param req {object}
- * @return {*|ResponseCookie}
- */
-function parseHostJWt(req) {
-	if (!(cookieKey in req.cookies)) {
-		throw Error(`cookie key '${cookieKey}' not found in request cookies`);
-	}
-
-	return req.cookies[cookieKey];
-}
-
-/**
- *
- * @param hostOauthId {string}
- * @return {Promise<void>}
- */
-async function verifyHost(hostOauthId) {
-	const isExist = await isExistHostOAuthId(hostOauthId);
-
-	if (!isExist) {
-		throw Error(`host's OauthId ${hostOauthId} is not verified`);
-	}
-}
 
 /**
  *
@@ -51,7 +24,7 @@ export default async function hostAuthenticate(req, res, next) {
 
 		const hostOauthId = payload.sub;
 
-		await verifyHost(hostOauthId);
+		verifyHostByOauthId(hostOauthId);
 
 		return res.redirect(routePage.host);
 	} catch (e) {
