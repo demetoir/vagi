d4c607658d1b9f92614b2b3982c8644f0d2cac8a
demetoir-destop <wnsqlehlswk@naver.com>
2020-04-03 12:31:45 +0900

    refactoring: minor

--- backend/node/graphQL/middlewares/authenticate.js
+++ backend/node/graphQL/middlewares/authenticate.js
@@ -1,25 +1,31 @@
 import {findHostByOAuthId} from "../../DB/queries/host.js";
 import logger from "../logger.js";
-import {
-	AUTHORITY_TYPE_GUEST,
-	AUTHORITY_TYPE_HOST,
-} from "../../constants/authorityTypes.js";
+import {AUTHORITY_TYPE_GUEST, AUTHORITY_TYPE_HOST} from "../../constants/authorityTypes.js";
+
+async function onHost(sub) {
+	const hostInfo = await findHostByOAuthId(sub);
+
+	return {sub: AUTHORITY_TYPE_HOST, info: hostInfo};
+}
+
+function onGuest(sub) {
+	return {sub: AUTHORITY_TYPE_GUEST, info: sub};
+}
 
 const authenticate = async (resolve, root, args, context, info) => {
+	// todo refactoring here
 	const audience = context.payload && context.payload.aud;
+	const sub = context.payload.sub;
 	let authority = {sub: null, info: null};
+
 
 	switch (audience) {
 		case AUTHORITY_TYPE_HOST: {
-			const hostInfo = await findHostByOAuthId(context.payload.sub);
-
-			authority = {sub: AUTHORITY_TYPE_HOST, info: hostInfo};
+			authority = await onHost(sub);
 			break;
 		}
 		case AUTHORITY_TYPE_GUEST: {
-			const guestInfo = context.payload.sub;
-
-			authority = {sub: AUTHORITY_TYPE_GUEST, info: guestInfo};
+			authority = onGuest(sub);
 			break;
 		}
 		default: {
