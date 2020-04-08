ef062e4c14394a2b28b71b684e4706080a03df62
Yujun Kim <wnsqlehlswk@naver.com>
2020-04-09 02:30:26 +0900

    add: host fixture

--- backend/node/spec/fixtures/HostFixtures.js
+++ backend/node/spec/fixtures/HostFixtures.js
@@ -1,6 +1,6 @@
 import {findOrCreateHostByOAuth} from "../../DB/queries/host.js";
 
-export default class GuestFixtures {
+export default class HostFixtures {
 	static async host(oauthId = null) {
 		const name = "name";
 		const image = "image";
