d6d291b9152695eea08690eec9b1c0018352634a
Yujun Kim <wnsqlehlswk@naver.com>
2020-04-09 01:59:48 +0900

    refactoring: better test describe text in express app

--- backend/node/spec/express/app.test.js
+++ backend/node/spec/express/app.test.js
@@ -5,8 +5,10 @@ import parse from "url-parse";
 import superTest from "supertest";
 import App from "../../express/app.js";
 import config from "../../express/config";
-import CookieJar from "../testHelper/CookieJar.js";
+import CookiePot from "../testHelper/CookiePot.js";
 import SequelizeTestHelper from "../testHelper/SequelizeTestHelper.js";
+import guestJWTCookie from "../../express/JWTCookie/guestJWTCookie";
+import {findOrCreateEvent} from "../../DB/queries/event.js";
 
 const app = App(config);
 const agent = superTest.agent(app);
@@ -16,6 +18,7 @@ describe(`express app`, () => {
 	const sequelizeMock = new SequelizeTestHelper();
 	const oauthId = "1234";
 
+	const cookiePot = new CookiePot();
 	const strategy = passport._strategies.google;
 
 	strategy._token_response = {
@@ -44,12 +47,13 @@ describe(`express app`, () => {
 
 	afterEach(async () => {
 		await sequelizeMock.dropAllAfterEach();
+		cookiePot.reset();
 	});
 
-	describe(`route  /auth/google/login`, () => {
+	describe(`get /auth/google/login`, () => {
 		const baseURL = "/auth/google/login";
 
-		it("should be able to redirect to AuthorizationURL ", async () => {
+		it("redirect to AuthorizationURL ", async () => {
 			const url = `${baseURL}`;
 
 			await agent.get(url).expect(res => {
@@ -63,12 +67,12 @@ describe(`express app`, () => {
 		});
 	});
 
-	describe(`route /auth/google/callback`, () => {
+	describe(`get /auth/google/callback`, () => {
 		const baseURL = "/auth/google/callback";
 
 		// todo 주소로 요청시 google auth는 authorization URL로 가서  oauth pass이후  이링크로 다시 오는 과정이 생략되었다..
 		// todo 그래서 원래 redirect location이 callbackURL이 아닌 authorization URL로 되어야한다
-		it("should be able to redirect callbackURL after oauth passed", async () => {
+		it("redirect callbackURL after oauth passed", async () => {
 			const url = `${baseURL}`;
 
 			await agent.get(url).expect(res => {
@@ -82,7 +86,7 @@ describe(`express app`, () => {
 			});
 		});
 
-		it("should be able to redirect host-app after jwt issued", async () => {
+		it("redirect host-app after jwt issued", async () => {
 			const url = `${baseURL}?__mock_strategy_callback=true`;
 
 			await agent.get(url).expect(res => {
@@ -97,29 +101,56 @@ describe(`express app`, () => {
 		});
 	});
 
-	describe("route `/guest`", () => {
-		describe("/guest", () => {
-			it("should be able redirect on jwt cookie not found", async () => {
-				const url = "/guest";
+	describe("get `/guest`", () => {
+		const url = "/guest";
 
+		describe("redirect to guest app", () => {
+			it("on success", async () => {
+				const event = await findOrCreateEvent({
+					eventName: "eventName",
+					eventCode: "eventcode",
+					HostId: null,
+				});
+				const eventId = event.id;
+				const guest = await createGuest(eventId);
+				const payload = {guestSid: guest.guestSid};
+				const accessToken = guestJWTCookie.sign(payload);
+
+				const payload = {};
+				guestJWTCookie.sign(payload);
+				const token = "malformed jwt";
+
+				cookiePot.set("vaagle-guest", token);
+
+				await agent.get(url).expect(res => {
+					assert.equal(res.status, 302);
+					assert.equal(res.headers.location, config.routePage.guest);
+				});
+			});
+		});
+
+		describe("redirect to main app", () => {
+			it("on jwt cookie not found", async () => {
 				await agent.get(url).expect(res => {
 					assert.equal(res.status, 302);
 					assert.equal(res.headers.location, config.routePage.main);
 				});
 			});
 
-			it("should be able redirect on jwt malformed", async () => {
-				const url = "/guest";
-				const cookieJar = new CookieJar();
+			it("on jwt malformed", async () => {
 				const token = "malformed jwt";
 
-				cookieJar.set("vaagle-guest", token);
+				cookiePot.set("vaagle-guest", token);
 
 				await agent
 					.get(url)
-					.set("Cookie", [cookieJar.toEncodedString()])
+					.set("Cookie", [cookiePot.toEncodedString()])
 					.expect(res => {
 						assert.equal(res.status, 302);
+						assert.equal(
+							res.headers.location,
+							config.routePage.main
+						);
 					});
 			});
 		});
