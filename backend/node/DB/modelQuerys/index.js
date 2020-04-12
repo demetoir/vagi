a87335e65d4fe5e4c77b01342bfb293d562661ac
demetoir-destop <wnsqlehlswk@naver.com>
2020-04-13 04:21:27 +0900

    chore: add todo

--- backend/node/DB/modelQuerys/index.js
+++ backend/node/DB/modelQuerys/index.js
@@ -5,3 +5,14 @@ import EventQuery from "./querys/EventQuery.js";
 export const hostQuery = new HostQuery(models);
 
 export const eventQuery = new EventQuery(models);
+
+
+// todo candidate
+// todo emoji
+// todo hashtag
+// todo host
+// todo like
+// todo poll
+// todo question
+// todo vote
+
