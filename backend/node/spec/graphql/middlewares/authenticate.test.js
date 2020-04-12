845acc4ec0f8ff90a40aa29bcc73d3389753a969
demetoir-destop <wnsqlehlswk@naver.com>
2020-04-13 04:01:01 +0900

    chore: add todo graphql middleware authenticate.js

---
+++ backend/node/spec/graphql/middlewares/authenticate.test.js
@@ -0,0 +1,66 @@
+import assert from "assert";
+import sinon from "sinon";
+import {describe, it} from "mocha";
+import SequelizeTestHelper from "../../testHelper/SequelizeTestHelper.js";
+import authenticate from "../../../graphQL/middlewares/authenticate.js";
+
+describe("graphql middleware authenticate", () => {
+	new SequelizeTestHelper().autoSetup();
+
+	const resolverSpy = sinon.spy();
+	const root = sinon.stub();
+	const args = sinon.stub();
+	const info = sinon.stub();
+
+	let context = {};
+
+	it("pass ", () => {
+		const a = authenticate();
+
+		assert(true);
+	});
+
+	describe("fail", () => {
+		it("on context authorization not exist", () => {
+			const a = authenticate();
+
+			// todo test
+			assert(true);
+		});
+
+		it("on jwt token cant not parse ", () => {
+			const a = authenticate();
+
+			// todo test
+			assert(true);
+		});
+
+		it("on jwt malformed", () => {
+			const a = authenticate();
+
+			// todo test
+			assert(true);
+		});
+
+		it("on invalid audience", () => {
+			const a = authenticate();
+
+			// todo test
+			assert(true);
+		});
+
+		it("on invalid host oauthId", () => {
+			const a = authenticate();
+
+			// todo test
+			assert(true);
+		});
+
+		it("on invalid guest guestSid", () => {
+			const a = authenticate();
+
+			// todo test
+			assert(true);
+		});
+	});
+});
