830bb28c4c70d4a492c6b8cc46f600ad2d1ca0e5
demetoir-destop <wnsqlehlswk@naver.com>
2020-04-13 04:12:34 +0900

    chore: add test socket.io middleware authenticate, validator verifyJWTPayload

---
+++ backend/node/socket_io_server/validator/verifyJWTPayload.js
@@ -0,0 +1,38 @@
+import {
+	AUTHORITY_TYPE_GUEST,
+	AUTHORITY_TYPE_HOST,
+} from "../../constants/authorityTypes.js";
+import {getGuestByGuestSid} from "../../DB/queries/guest.js";
+import {findHostByOAuthId} from "../../DB/queries/host.js";
+import loadConfig from "../config/configLoader.js";
+
+const {tokenArgs} = loadConfig();
+
+const isInValidAud = aud =>
+	aud !== AUTHORITY_TYPE_GUEST && aud !== AUTHORITY_TYPE_HOST;
+
+const isInValidIss = iss => iss !== tokenArgs.issuer;
+
+export default async function verifyJWTPayload(payload) {
+	const {aud, iss} = payload;
+
+	if (isInValidIss(iss)) {
+		throw new Error("Authentication Error: invalid iss");
+	}
+
+	if (isInValidAud(aud)) {
+		throw new Error("Authentication Error: invalid aud");
+	}
+
+	let user = null;
+
+	if (aud === "guest") {
+		user = getGuestByGuestSid(payload.guestSid);
+	} else if (aud === "host") {
+		user = findHostByOAuthId(payload.oauthId);
+	}
+
+	if (!user) {
+		throw new Error("Authentication Error: invalid userInfo");
+	}
+}
