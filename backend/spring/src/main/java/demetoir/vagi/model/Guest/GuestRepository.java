5eaef1797fa293990e385e0669985456e8c8bf93
demetoir-destop <wnsqlehlswk@naver.com>
2020-04-13 22:53:30 +0900

    add: resolve todo GuestRepository test

--- backend/spring/src/main/java/demetoir/vagi/model/Guest/GuestRepository.java
+++ backend/spring/src/main/java/demetoir/vagi/model/Guest/GuestRepository.java
@@ -3,12 +3,12 @@ package demetoir.vagi.model.Guest;
 import demetoir.vagi.model.Event.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Set;
+import java.util.List;
+import java.util.Optional;
 
 public interface GuestRepository extends JpaRepository<Guest, Integer> {
-  // todo test
-  Guest findByGuestSid(String guestSid);
 
-  // todo test
-  Set<Guest> findByEvent(Event event);
+  <S extends Guest> Optional<S> findOneByGuestSid(String guestSid);
+
+  List<Guest> findAllByEvent(Event event);
 }
--- backend/spring/src/test/java/demetoir/vagi/model/Guest/GuestRepositoryTest.java
+++ backend/spring/src/test/java/demetoir/vagi/model/Guest/GuestRepositoryTest.java
@@ -1,7 +1,9 @@
 package demetoir.vagi.model.Guest;
 
 import demetoir.vagi.config.JpaAuditingConfig;
+import demetoir.vagi.model.Event.Event;
 import demetoir.vagi.model.Event.EventRepository;
+import lombok.extern.java.Log;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
@@ -10,8 +12,12 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.sql.Timestamp;
+import java.util.Date;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
+@Log
 @RunWith(SpringRunner.class)
 @Import(JpaAuditingConfig.class)
 @DataJpaTest
@@ -30,38 +36,54 @@ class GuestRepositoryTest {
   }
 
   @Test
-  void createGuest() {
-    // todo
+  void findOneByGuestSid_return_entity() {
+    String guestSid = "guest sid";
+    var newGuest = Guest.builder().guestSid(guestSid).build();
 
+    var existGuest = guestRepository.save(newGuest);
+
+    var guestOptional = guestRepository.findOneByGuestSid(guestSid);
+    assertThat(guestOptional.isPresent()).isTrue();
+
+    var guest = guestOptional.get();
+    assertThat(guest).isEqualTo(existGuest);
   }
 
   @Test
-  void findGuestById() {
-    // todo
+  void findOneByGuestSid_return_null_on_not_exist() {
+    String guestSid = "guest sid";
 
+    var guestOptional = guestRepository.findOneByGuestSid(guestSid);
+    assertThat(guestOptional.isPresent()).isFalse();
   }
 
   @Test
-  void findByGuestSid() {
-    // todo
+  void findAllByEvent() {
+    var eventName = "eventName";
+    var endAt = new Timestamp(new Date().getTime());
+    var startAt = new Timestamp(new Date().getTime());
+    var eventCode = "eventCode";
 
-  }
+    var newEvent =
+        Event.builder()
+            .eventName(eventName)
+            .eventCode(eventCode)
+            .startAt(startAt)
+            .endAt(endAt)
+            .build();
 
-  @Test
-  void associateWithEvent() {
-    // todo
+    String guestSid = "guest sid";
+    var newGuest = Guest.builder().guestSid(guestSid).build();
 
-  }
+    newGuest.setEvent(newEvent);
+    newEvent.getGuests().add(newGuest);
 
-  @Test
-  void autoCreatedAT() {
-    // todo
+    var existGuest = guestRepository.save(newGuest);
+    var existEvent = eventRepository.save(newEvent);
 
-  }
+    var resultGuests = guestRepository.findAllByEvent(existEvent);
 
-  @Test
-  void autoUpdatedAt() {
-    // todo
-
+    assertThat(resultGuests.size()).isEqualTo(1);
+    assertThat(resultGuests.get(0)).isEqualTo(existGuest);
   }
 }
