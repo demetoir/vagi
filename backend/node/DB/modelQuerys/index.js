01dd7ce2e7396df210bde28cbf8432be8e22e5c1
demetoir-destop <wnsqlehlswk@naver.com>
2020-04-13 18:15:10 +0900

    enhance: resolve todo GuestQuery

--- backend/node/DB/modelQuerys/index.js
+++ backend/node/DB/modelQuerys/index.js
@@ -1,10 +1,13 @@
 import HostQuery from "./querys/HostQuery.js";
 import models from "../models";
 import EventQuery from "./querys/EventQuery.js";
+import GuestQuery from "./querys/GuestQuery.js";
 
 export const hostQuery = new HostQuery(models);
 
 export const eventQuery = new EventQuery(models);
+
+export const guestQuery = new GuestQuery(models);
 
 
 // todo candidate
