30b0b7be4cc19a3d3e46b24f121d893552fb3894
demetoir-destop <wnsqlehlswk@naver.com>
2020-04-13 21:22:06 +0900

    chore: add jsdoc DB/utils.js

--- backend/node/DB/utils.js
+++ backend/node/DB/utils.js
@@ -1,3 +1,8 @@
+/**
+ *
+ * @param res
+ * @return {object|null}
+ */
 export function plainOne(res) {
 	if (res !== null) {
 		return res.get({plain: true});
@@ -6,10 +11,20 @@ export function plainOne(res) {
 	return res;
 }
 
+/**
+ *
+ * @param res
+ * @return {object[]}
+ */
 export function plainFindAll(res) {
 	return res.map(x => x.get({plain: true}));
 }
 
+/**
+ *
+ * @param res
+ * @return {[object, Boolean]}
+ */
 export function plainFindOrCreate(res) {
 	return [res[0].get({plain: true}), res[1]];
 }
