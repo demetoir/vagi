c1473983b4585cab573c7b9be6515fe1f7f37232
Yujun Kim <wnsqlehlswk@naver.com>
2020-04-14 02:20:21 +0900

    chore: add RedisJSONCache todos

---
+++ backend/node/spec/redis/RedisJSonCache.test.js
@@ -0,0 +1,23 @@
+import sinon from "sinon";
+import assert from "assert";
+import {describe, it} from "mocha";
+
+describe("RedisJSONCache", () => {
+	const onMissSpy = sinon.spy();
+
+	// todo test
+	it("construct", () => {
+		assert(true);
+	});
+
+	describe("set", () => {
+		// todo test
+		it("should ", () => {});
+	});
+	describe("get", () => {
+		// todo test
+		it("on hit ", () => {});
+		// todo test
+		it("on miss ", () => {});
+	});
+});
