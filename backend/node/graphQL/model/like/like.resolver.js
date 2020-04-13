4293fc36b6261a0c048a25a69d4ade9c32f68705
Yujun Kim <wnsqlehlswk@naver.com>
2020-04-14 01:55:14 +0900

    chore: add todo

--- backend/node/graphQL/model/like/like.resolver.js
+++ backend/node/graphQL/model/like/like.resolver.js
@@ -1,5 +1,6 @@
 import {getLikesByGuestId} from "../../../DB/queries/like.js";
 
+// todo add auth guest
 const didILikesResolver = async (_, {GuestId}) => getLikesByGuestId(GuestId);
 
 export default {
