62433b2b6094f237e5de9f675655fc93fe72716e
demetoir-destop <wnsqlehlswk@naver.com>
2020-04-15 00:48:04 +0900

    enhance: inject domain env at backend node

--- backend/node/express/config/express.test.config.js
+++ backend/node/express/config/express.test.config.js
@@ -5,6 +5,7 @@ dotenv.config();
 const env = process.env;
 
 const config = {
+	hostDomain: env.TEST_HOST_DOMAIN,
 	port: env.EXPRESS_TEST_PORT,
 	publicPath: env.EXPRESS_TEST_PUBLIC_PATH,
 	tokenArgs: {
@@ -12,15 +13,15 @@ const config = {
 		issuer: "auth_token_issuer",
 		audience: "auth_token_audience",
 	},
+	oAuthArgs: {
+		clientID: "oauth2_client_dev_id",
+		clientSecret: "oauth2_client_dev_secret",
+		callbackURL: `http://${env.TEST_HOST_DOMAIN}${process.env.TEST_OAUTH2_CALLBACK}`,
+	},
 	routePage: {
 		main: "http://test/main",
 		host: "http://test/host",
 		guest: "http://test/guest",
-	},
-	oAuthArgs: {
-		clientID: "oauth2_client_dev_id",
-		clientSecret: "oauth2_client_dev_secret",
-		callbackURL: "http://localhost:3001/auth/google/callback",
 	},
 };
 
